0a843e526222ca134f271aa3fab310dc0d3a6697
kuolbuom <127087268+kuolbuom@users.noreply.github.com>
2023-05-30 20:58:28 -1200

    ❤my first commites

---
+++ practice.js
@@ -0,0 +1,23 @@
+
+
+if (time <11 ) {
+  greeting = 'good morning';
+}
+
+else if (time <3 ){
+  greeting = 'good afternoon';
+}
+
+else {
+    greeting = 'good evening';
+}
+console.log(time)
+
+let name ='mosh';
+console.log(name)
+
+let x =60;
+let y =60;
+let m =40;
+z =x+y+m;
+console.log(z)
---
+++ variable.js
@@ -0,0 +1,49 @@
+let number1=1635
+
+// this is variable 2
+let number2=564
+
+// this is my result
+let result
+
+// initialization
+number3=100
+number4=300
+
+// initialize
+result = number1+number2
+console.log(result)
+
+// calculate the product of two numbers in javaScript
+var num1=4
+var num2=6
+
+// initialize
+result = num1*num2
+console.log(result)
+
+let m1 =300
+let m2 =400
+let m3 =500
+let result =m1+m2+m3
+console.log(result)
+
+let name ='kuol ';
+let greeting ='hi Am buom patai !';
+let result =name+greeting
+console.log(result)
+var fName ='Adom ';
+var sName ='Omon ';
+var lName ='yamy ';
+var fullName =fName+sName+lName
+console.log(fullName)
+
+let name ="Daudi ";
+let name1 ="Achut ";
+let name2 ="lenoto ";
+let name3 ="kuol ";
+let name4 ="Allan ";
+let name5 ="john ";
+let name6 ="mohamed";
+let allname =name+name1+name2+name3+name4+name5+name6;
+console.log(allname);
